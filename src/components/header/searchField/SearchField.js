ad5d167e5a14e4a31505beb6f053afc90abb95b3
ramkumar2098 <ramkumar2098@protonmail.com>
2020-07-23 18:39:16 +0530

    fixed searchField dropdown

--- src/components/header/searchField/SearchField.js
+++ src/components/header/searchField/SearchField.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import SlashSVG from 'SVGs/SlashSVG';
 import { searchDropdownItems } from 'data/searchDropdownItems';
 import DropdownItem from './dropdownItem/DropdownItem';
@@ -14,24 +14,91 @@ function SearchField({ styles, searchQuery, setSearchQuery }) {
     marginBottom: styles.marginBottom,
   };
 
+  const [mouseOverDropdown, setMouseOverDropdown] = useState(false);
+
+  const inputRef = useRef();
+  const jumpToRef = useRef();
+
+  useEffect(() => {
+    if (!mouseOverDropdown && searchQuery.trim()) {
+      const dropdownItems = document.querySelectorAll('.dropdownItem');
+      dropdownItems.forEach(dropdownItem =>
+        dropdownItem.classList.remove(style.highlight)
+      );
+
+      dropdownItems[0].classList.add(style.highlight);
+    }
+  }, [searchQuery]);
+
+  useEffect(() => {
+    inputRef.current.addEventListener('keyup', e => {
+      if (e.keyCode === 40 || e.keyCode === 38) {
+        const dropdownItems = [...document.querySelectorAll('.dropdownItem')];
+        const highlightedElement = dropdownItems.filter(dropdownItem =>
+          dropdownItem.classList.contains(style.highlight)
+        );
+
+        const indexOfHighlightedElement = dropdownItems.indexOf(
+          highlightedElement[0]
+        );
+
+        const highlight = index => {
+          dropdownItems[indexOfHighlightedElement].classList.remove(
+            style.highlight
+          );
+          dropdownItems[index].classList.add(style.highlight);
+        };
+
+        return e.keyCode === 40
+          ? dropdownItems.length === 1
+            ? dropdownItems[0].classList.add(style.highlight)
+            : indexOfHighlightedElement > -1 &&
+              indexOfHighlightedElement < dropdownItems.length - 1
+            ? highlight(indexOfHighlightedElement + 1)
+            : null
+          : e.keyCode === 38 && indexOfHighlightedElement > 0
+          ? highlight(indexOfHighlightedElement - 1)
+          : null;
+      } else if (e.keyCode === 27) {
+        inputRef.current.blur();
+      }
+    });
+  }, []);
+
   return (
     <span className={style.searchField} style={searchField}>
       <input
         type="text"
+        ref={inputRef}
         value={searchQuery}
-        placeholder="Search or jump to..."
         onChange={e => setSearchQuery(e.target.value)}
+        placeholder="Search or jump to..."
         className={style.input}
         style={input}
       />
-      <SlashSVG />
+      <span onClick={() => inputRef.current.focus()} className={style.slash}>
+        <SlashSVG />
+      </span>
       <div className={style.dropdown}>
-        <div>
+        <div
+          onMouseOver={() => setMouseOverDropdown(true)}
+          onMouseOut={() => setMouseOverDropdown(false)}
+        >
           {searchDropdownItems.map(dropdownItem => (
             <DropdownItem
               key={dropdownItem.badge}
               dropdownItem={dropdownItem}
               searchQuery={searchQuery}
+              jumpToRef={jumpToRef}
+              removeHighlight={() => {
+                const dropdownItems = document.querySelectorAll(
+                  '.dropdownItem'
+                );
+                dropdownItems.forEach(dropdownItem =>
+                  dropdownItem.classList.remove(style.highlight)
+                );
+              }}
+              addHighlight={e => e.currentTarget.classList.add(style.highlight)}
             />
           ))}
         </div>
@@ -41,5 +108,3 @@ function SearchField({ styles, searchQuery, setSearchQuery }) {
 }
 
 export default SearchField;
-
-// slash onClick focus
--- src/components/header/searchField/dropdownItem/DropdownItem.js
+++ src/components/header/searchField/dropdownItem/DropdownItem.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import style from './DropdownItem.module.css';
 
-function DropdownItem({ dropdownItem, searchQuery }) {
+function DropdownItem({
+  dropdownItem,
+  searchQuery,
+  jumpToRef,
+  removeHighlight,
+  addHighlight,
+}) {
   const { Icon, badge } = dropdownItem;
 
   return (
-    (badge === 'Jump to' || searchQuery) && (
-      <a href="#" className={style.dropdownItem + ' dropdownItem'}>
+    ((badge === 'Jump to' &&
+      'facebook/react'.includes(searchQuery.trim().toLowerCase())) ||
+      (badge !== 'Jump to' && searchQuery.trim())) && (
+      <a
+        href="#"
+        ref={badge === 'Jump to' ? jumpToRef : null}
+        onMouseOver={removeHighlight}
+        onMouseOut={addHighlight}
+        className={style.dropdownItem + ' dropdownItem'}
+      >
         <Icon />
         <span className={style.searchQuery}>
           {badge === 'Jump to' ? 'facebook/react' : searchQuery}
