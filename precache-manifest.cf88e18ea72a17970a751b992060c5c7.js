74868cce107f244c5064a89b9f90f184e8eaf8f9
ramkumar2098 <ramkumar2098@protonmail.com>
2020-07-28 18:58:51 +0530

    Updates

---
+++ precache-manifest.cf88e18ea72a17970a751b992060c5c7.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ccc0f1f0b7b658abe858788721142f6",
+    "url": "/github-issues-page-clone/index.html"
+  },
+  {
+    "revision": "e25f9b96ea190316ee6c",
+    "url": "/github-issues-page-clone/static/css/main.b92917d4.chunk.css"
+  },
+  {
+    "revision": "88acd7f40e731643beaa",
+    "url": "/github-issues-page-clone/static/js/2.481650f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-issues-page-clone/static/js/2.481650f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e25f9b96ea190316ee6c",
+    "url": "/github-issues-page-clone/static/js/main.168a0ac0.chunk.js"
+  },
+  {
+    "revision": "9558a987385c3dd5bd20",
+    "url": "/github-issues-page-clone/static/js/runtime-main.69818d8d.js"
+  },
+  {
+    "revision": "57cbf21d0fe46616375a16233914ddaa",
+    "url": "/github-issues-page-clone/static/media/10137.57cbf21d.jpeg"
+  },
+  {
+    "revision": "d6d67b7fab507718056c65a4f40f1789",
+    "url": "/github-issues-page-clone/static/media/1131998.d6d67b7f.jpeg"
+  },
+  {
+    "revision": "9a2064cce876a758e5d65a4fb1e1251c",
+    "url": "/github-issues-page-clone/static/media/1145515.9a2064cc.jpeg"
+  },
+  {
+    "revision": "525266aa450b352ee2f0ff6a0bb4dfe6",
+    "url": "/github-issues-page-clone/static/media/1319001.525266aa.jpeg"
+  },
+  {
+    "revision": "e3169c8967631b650ff5c4a2c82815b5",
+    "url": "/github-issues-page-clone/static/media/143613.e3169c89.jpeg"
+  },
+  {
+    "revision": "d12cc1ba730c792ce20870d2a13bd09a",
+    "url": "/github-issues-page-clone/static/media/1613874.d12cc1ba.jpeg"
+  },
+  {
+    "revision": "9724f2bc15a45b69b93c547885890ef7",
+    "url": "/github-issues-page-clone/static/media/17268815.9724f2bc.jpeg"
+  },
+  {
+    "revision": "c2764fedb554972b3fb7dd435383ad85",
+    "url": "/github-issues-page-clone/static/media/1942967.c2764fed.jpeg"
+  },
+  {
+    "revision": "e771f766f444d0bcdc9cd30772d8ec1d",
+    "url": "/github-issues-page-clone/static/media/1966349.e771f766.jpeg"
+  },
+  {
+    "revision": "dc727992b4982cfb6865e3c713b3ac10",
+    "url": "/github-issues-page-clone/static/media/19931582.dc727992.jpeg"
+  },
+  {
+    "revision": "5e1b6f9bd80dfcbfc1ec175ebfcb69fa",
+    "url": "/github-issues-page-clone/static/media/2101409.5e1b6f9b.png"
+  },
+  {
+    "revision": "b9af9a4d1480e9109dea095bdb02f0d4",
+    "url": "/github-issues-page-clone/static/media/2177796.b9af9a4d.jpeg"
+  },
+  {
+    "revision": "c42ebb627858e629d400883bc7c719b1",
+    "url": "/github-issues-page-clone/static/media/2292388.c42ebb62.png"
+  },
+  {
+    "revision": "2fe7788d9d01780c18d4026f56e6af5f",
+    "url": "/github-issues-page-clone/static/media/231343.2fe7788d.png"
+  },
+  {
+    "revision": "653d70ead90c130cfc1b6a3eb7e50776",
+    "url": "/github-issues-page-clone/static/media/23149166.653d70ea.png"
+  },
+  {
+    "revision": "dbee85a437fb3f1bef88464e2608c5e7",
+    "url": "/github-issues-page-clone/static/media/23192489.dbee85a4.jpeg"
+  },
+  {
+    "revision": "cea5eed1abe9cb8c45a6ccfce936ee44",
+    "url": "/github-issues-page-clone/static/media/2646229.cea5eed1.png"
+  },
+  {
+    "revision": "22e57dbce881eb29ed6eb450a869fea0",
+    "url": "/github-issues-page-clone/static/media/29597.22e57dbc.jpeg"
+  },
+  {
+    "revision": "2ba838baec4a0fb3f0a6587662e27f60",
+    "url": "/github-issues-page-clone/static/media/2982247.2ba838ba.jpeg"
+  },
+  {
+    "revision": "fb047df7fbb3274df89fa9351f59d2bc",
+    "url": "/github-issues-page-clone/static/media/30293804.fb047df7.jpeg"
+  },
+  {
+    "revision": "43a50fdca290ea4ede2b092525a1f772",
+    "url": "/github-issues-page-clone/static/media/3090066.43a50fdc.jpeg"
+  },
+  {
+    "revision": "68cbdbf2a9f0ff4aac3e32b391459263",
+    "url": "/github-issues-page-clone/static/media/31639964.68cbdbf2.jpeg"
+  },
+  {
+    "revision": "83738c36cf868b4729baf74f9511d6b9",
+    "url": "/github-issues-page-clone/static/media/3624098.83738c36.jpeg"
+  },
+  {
+    "revision": "024061e1db9110a0f314115a25d19c1f",
+    "url": "/github-issues-page-clone/static/media/396236.024061e1.jpeg"
+  },
+  {
+    "revision": "eefd3ca29701e028869dc4e273cc70b8",
+    "url": "/github-issues-page-clone/static/media/4028904.eefd3ca2.jpeg"
+  },
+  {
+    "revision": "ebad4296a6211b0df8cd55e91b3f9ffe",
+    "url": "/github-issues-page-clone/static/media/43141484.ebad4296.jpeg"
+  },
+  {
+    "revision": "61412789aaa8b93bdfba584ee208c0c7",
+    "url": "/github-issues-page-clone/static/media/4465755.61412789.jpeg"
+  },
+  {
+    "revision": "f9e5a0db6cb523a3b2a5ac1524366f7f",
+    "url": "/github-issues-page-clone/static/media/456610.f9e5a0db.jpeg"
+  },
+  {
+    "revision": "7652350a81d0db13157986e8f416e39f",
+    "url": "/github-issues-page-clone/static/media/47941658.7652350a.jpeg"
+  },
+  {
+    "revision": "2fdeaf95929c3d4846e392f2c2ee768e",
+    "url": "/github-issues-page-clone/static/media/545965.2fdeaf95.jpeg"
+  },
+  {
+    "revision": "f1cf401bc9ad944de107d18612037a7d",
+    "url": "/github-issues-page-clone/static/media/558411.f1cf401b.jpeg"
+  },
+  {
+    "revision": "153ca9e5b96b6483b3309d0ef8cf3d8c",
+    "url": "/github-issues-page-clone/static/media/563999.153ca9e5.jpeg"
+  },
+  {
+    "revision": "51523b98f8d0b0d6ebd03e8580e5bb20",
+    "url": "/github-issues-page-clone/static/media/634414.51523b98.png"
+  },
+  {
+    "revision": "3be240af4afcc1ce1ef0ac64280e1a61",
+    "url": "/github-issues-page-clone/static/media/69631.3be240af.png"
+  },
+  {
+    "revision": "1b3cad0807549ac5b221a7ad8cfc6a53",
+    "url": "/github-issues-page-clone/static/media/78122.1b3cad08.jpeg"
+  },
+  {
+    "revision": "4bd1c67dcbea2e0d561b984f57547439",
+    "url": "/github-issues-page-clone/static/media/8156902.4bd1c67d.png"
+  },
+  {
+    "revision": "cb6ebbed8dfbb0cb89694d262d601754",
+    "url": "/github-issues-page-clone/static/media/850754.cb6ebbed.jpeg"
+  },
+  {
+    "revision": "58e26e0089a22439f154c849cd93f5ae",
+    "url": "/github-issues-page-clone/static/media/856742.58e26e00.jpeg"
+  },
+  {
+    "revision": "e3957c0099dd54dd8384d3469579ef9e",
+    "url": "/github-issues-page-clone/static/media/865195.e3957c00.jpeg"
+  },
+  {
+    "revision": "ab31dc1716b01e074c8b8573411d5fb7",
+    "url": "/github-issues-page-clone/static/media/921609.ab31dc17.jpeg"
+  },
+  {
+    "revision": "96b254d6feaf18aba33c70ed4ee6a6ce",
+    "url": "/github-issues-page-clone/static/media/940133.96b254d6.jpeg"
+  },
+  {
+    "revision": "b265b068713f628380d55064f5061afa",
+    "url": "/github-issues-page-clone/static/media/97944.b265b068.jpeg"
+  }
+]);
