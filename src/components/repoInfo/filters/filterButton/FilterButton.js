5f8566f7e29bf2056302e467d83b7df3ae5e1d93
ramkumar2098 <ramkumar2098@protonmail.com>
2020-07-28 18:48:07 +0530

    completed

--- src/components/repoInfo/filters/filterButton/FilterButton.js
+++ src/components/repoInfo/filters/filterButton/FilterButton.js
@@ -3,18 +3,18 @@ import Overlay from 'components/overlay/Overlay';
 import DropdownCaret from 'components/dropdownCaret/DropdownCaret';
 import style from './FilterButton.module.css';
 
-function FilterButton({ displayModal, openMenu }) {
+function FilterButton({ displayMenu, openMenu }) {
   const filterBtnRef = useRef();
 
   useEffect(() => {
-    displayModal
+    displayMenu
       ? (filterBtnRef.current.style.backgroundColor = '#f3f4f6')
       : (filterBtnRef.current.style.backgroundColor = '#fafbfc');
-  }, [displayModal]);
+  }, [displayMenu]);
 
   return (
     <>
-      {displayModal && <Overlay />}
+      {displayMenu && <Overlay />}
       <button
         ref={filterBtnRef}
         onClick={openMenu}
